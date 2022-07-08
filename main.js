a551a7179902493c93318150b5be11a1456bcb99
Mahasharanyamandava <91941363+Mahasharanyamandava@users.noreply.github.com>
2022-07-08 20:00:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload()
+{
+}
+
+
+
+   function setup()
+    { 
+        canvas = createCanvas(640, 480);
+        
+        canvas.position(110, 250);
+    }
+         
+         
+         
+
+    function draw()
+    {
+      fill(204,0,0);
+        stroke(204,0,0);
+        circle(30, 30, 40);
+        fill(0,120,0);
+        stroke(0,120,0);
+        rect(100, 100, 55, 55, 20);
+        fill(0,0,130);
+        stroke(0,0,130);
+        ellipse(150, 200, 55, 55);
+    
+        
+
+    }
